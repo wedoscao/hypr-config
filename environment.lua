a923bce41ef7152b8d97603a46def1c11d322385
Sang H. Cao <hoaisang0806@gmail.com>
2026-05-24 15:20:13 +0700

    fix: lua create '$HOME' dir instead of expanding path

--- environment.lua
+++ environment.lua
@@ -6,5 +6,5 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("HYPRSHOT_DIR", "$HOME/Pictures/Screenshots")
+hl.env("HYPRSHOT_DIR", os.getenv("HOME") .. "/Pictures/Screenshots")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
